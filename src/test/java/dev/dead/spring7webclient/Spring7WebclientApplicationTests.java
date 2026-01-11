308fb2c0e3a8fb0c6984956d11a6776c908c034f
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-01-11 16:38:41 +0300

    init

---
+++ src/test/java/dev/dead/spring7webclient/Spring7WebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7WebclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
